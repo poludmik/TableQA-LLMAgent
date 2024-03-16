d366e76932878a8abf56c7bdff9c751497a4b96f
poludmik <michael.poludin@gmail.com>
2024-03-16 11:34:03 +0100

    Bug fixes (changes to df, return in code regex)

--- finetuning/lora/predict.py
+++ finetuning/lora/predict.py
@@ -20,7 +20,7 @@ model.config.use_cache = False
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 peft_model_id = "finetuning/lora/cp/codellama_python/final"
-peft_model = PeftModel.from_pretrained(model, peft_model_id, offload_folder="finetuning/lora/offload/test_opt")
+peft_model = PeftModel.from_pretrained(model, peft_model_id, offload_folder="finetuning/lora/offload/codellama_python")
 # peft_model = model
 
 repo = "poludmik/code_completion_for_data_analysis"
@@ -33,19 +33,45 @@ train_dataset = split_dataset['train']
 validation_dataset = small_dataset
 
 
-input_prompt = train_dataset[2]["input"] + "# Code:\n    "
+input_prompt = '''```python
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def solve(df: pd.DataFrame):
+    """ Function to solve the user query: 'Select rows where temperature is greater than 30 and starting SoC is lower than 25.'.
+
+    DataFrame `df` is fixed. The resut of `print(df.head(2))` is:
+           ChargingCycleId CarType  SoC_Start (pc)  SoC_End (pc)  Temperature (C)  Current (A)  Temperature (C) - SoC_Start (pc)
+    0                1  Taycan              21            24               12          160                                -9
+    1                2  Taycan              26            52               22           10                                -4
+
+    Here is also a list of column names along with the first sample values for your convenience (each column is represented as a separate json object within a list):
+    [{'column_name': 'ChargingCycleId', 'sample_values': [1, 2]}, {'column_name': 'CarType', 'sample_values': ['Taycan', 'Taycan']}, {'column_name': 'SoC_Start (pc)', 'sample_values': [21, 26]}, {'column_name': 'SoC_End (pc)', 'sample_values': [24, 52]}, {'column_name': 'Temperature (C)', 'sample_values': [12, 22]}, {'column_name': 'Current (A)', 'sample_values': [160, 10]}, {'column_name': 'Temperature (C) - SoC_Start (pc)', 'sample_values': [-9, -4]}]
+
+
+    Args:
+        df: pandas DataFrame
+
+    Returns:
+        Variable containing the answer to the task 'Select rows where temperature is greater than 30 and starting SoC is lower than 25.' (typed e.g. float, DataFrame, list, string, dict, etc.).
+    """
+    # Code:\n    '''
+
+# input_prompt = train_dataset[2]["input"] + "# Code:\n    "
 # input_prompt = """\"I Am Curious: Yellow\" is a risible and pretentious steaming"""
 input_tokens = tokenizer(input_prompt, return_tensors="pt")["input_ids"].to("cuda")
 with torch.cuda.amp.autocast():
     generation_output = peft_model.generate(
         input_ids=input_tokens,
-        max_new_tokens=100,
+        max_new_tokens=300,
         do_sample=True,
         top_k=10,
         top_p=0.9,
-        temperature=0.3,
-        repetition_penalty=1.15,
-        num_return_sequences=1,
+        temperature=1e-9,
+        # repetition_penalty=1.1, # needs to be > 1?
+        # num_return_sequences=1, # no effect
         eos_token_id=tokenizer.eos_token_id,
       )
 
